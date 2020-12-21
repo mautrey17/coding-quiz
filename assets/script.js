b1d51748fe919dd0168eee555b28965e3d899029
Matt Autrey <mautrey17@gmail.com>
2020-12-20 20:04:25 -0500

    most things moved into functions, not all

--- assets/script.js
+++ assets/script.js
@@ -7,72 +7,52 @@ var startButton = document.querySelector("#start-button");
 var initialScreen = document.querySelector("#initial-screen");
 var rightOrWrong = document.querySelector("#area-if-rightWrong")
 var endTimer = false;
+var currentQuestion = document.querySelector("#current-question")
+var answerChoices = document.querySelector("#answer-choices")
 
 //Questions with options
 var questionOne = {
     q: "This is a sample question",
-    a: 3,
+    answer: 3,
     options: ["This is false 1", "This is false 2", "This is false 3", "This is true"]
 };
 var questionTwo = {
     q: "This is the second question",
-    a: 0,
+    answer: 0,
     options: ["This is true now", "this is false 1", "this is false 2", "this is false 3"]
 }
 
 //List of questions
-var questionList = [questionOne];
+var questionList = [questionOne, questionTwo];
 
 timer.textContent = score;
 
 startButton.addEventListener("click", function(event){
     if(event.target.matches("button")){
 
+        //set time to max and begin countdown
+        score = 15
+        scoreCountDown();
 
         //Hide starting screen and switch to questions
         initialScreen.style.display = "none";
         questionCount.style.display = "block";
         questionBlock.style.display = "block";
 
-        //set time to max and begin countdown
-        score = 15
-        scoreCountDown();
+        
 
         //Show questions
-        displayQuestion();
+        displayQuestion(0);
+        displayAnswers(0);
+
+        // displayQuestion(1);
+        // displayAnswers(1);
 
 
-        //for loop to genereate buttons for the questions
-        for(i=0; i < questionList[0].options.length; i++){
-            console.log("test");
-            var newButtonEl = document.createElement("button");
-            newButtonEl.textContent = questionList[0].options[i];
-            newButtonEl.setAttribute("class", "btn btn-info answer-choices");
-
-            //data value to determine which button is clicked
-            newButtonEl.setAttribute("data-value", i);
-            questionBlock.appendChild(newButtonEl);
-
-            //event listener for the button
-            newButtonEl.addEventListener("click", function(event){
-                if(event.target.matches("button")){
-                    var rightAnswer = parseInt(event.target.getAttribute("data-value"));
-                    console.log(rightAnswer);
-                    if(rightAnswer === questionList[0].a){
-                        //function to countdown correct being shown
-                        correctCD();
-                        
-                    }
-                    else{
-                        //function to countdown incorrect being shown
-                        incorrectCD();
-                    }
-                }
-            })
         
-        }
 
     }
+    
 });
 
 function scoreCountDown() {
@@ -90,11 +70,46 @@ function scoreCountDown() {
     }, 1000);
 }
 
-function displayQuestion(){
-    var newQuestionEl = document.createElement("h3");
-    newQuestionEl.textContent = questionList[0].q;
-    questionBlock.appendChild(newQuestionEl);
+function displayQuestion(qnum){
+    currentQuestion.textContent = questionList[qnum].q;
 }
+
+function displayAnswers(qnum){
+    //for loop to genereate buttons for the questions
+    for(i=0; i < questionList[qnum].options.length; i++){
+        console.log("test");
+        var newButtonEl = document.createElement("button");
+        newButtonEl.textContent = questionList[qnum].options[i];
+        newButtonEl.setAttribute("class", "btn btn-info answer-choices");
+
+        //data value to determine which button is clicked
+        newButtonEl.setAttribute("data-value", i);
+        answerChoices.appendChild(newButtonEl);
+
+        //event listener for the button
+        newButtonEl.addEventListener("click", function(event){
+            if(event.target.matches("button")){
+                var rightAnswer = parseInt(event.target.getAttribute("data-value"));
+                console.log(rightAnswer);
+                answerChoices.remove();
+                if(rightAnswer === questionList[qnum].answer){
+                    //function to countdown correct being shown
+                    correctCD();
+                    
+                }
+                else{
+                    //function to countdown incorrect being shown
+                    incorrectCD();
+                }
+                displayQuestion(parseInt(qnum)+1);
+                displayAnswers(qnum+1);
+            }
+        })
+    
+    }
+}
+
+
 
 function correctCD() {
     var display = 3;
